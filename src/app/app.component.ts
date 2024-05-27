9869e48f01deb4056e51c5bc9b575bcb3e4fc459
Sailendra prabu <a.sailendraprabu@gmail.com>
2024-05-27 13:23:56 +0000

    loader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { MenuController, Platform, ToastController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { LoadingController, MenuController, Platform, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'About', url: '/about', icon: 'information-circle' },
@@ -18,13 +18,36 @@ export class AppComponent {
   public query: string = '';
   public torrents: any[] = [];
   public menuActive: boolean = false;
+  searchResults: string[] = ['tsetset','sdfsdf','sdfsdf','test'];
+  showSuggestions: boolean = false;
+  loading!:any
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform, private loadingCtrl: LoadingController) {
     this.initializeApp();
   }
 
-  searchTorrents() {
+  async ngOnInit(): Promise<void> {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Dismissing after 3 seconds...',
+      // backdropDismiss: true
+    });
+  }
 
+ 
+
+  searchTorrents(event: any) {
+    const query = event.target.value.trim();
+    if(query.length > 0) {
+this.loading.present();
+    }
+    else {
+      this.loading.dismiss();
+    }
+    this.showSuggestions = query.length > 0;
+  }
+
+  selectSuggestion(suggestion: string) {
+    // Handle when a suggestion is selected, e.g., update the search input with the suggestion
   }
 
   initializeApp() {
