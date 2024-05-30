62473ecd5a1b932e9472203ef6e9d956f7348fd9
Sailendra prabu <a.sailendraprabu@gmail.com>
2024-05-31 01:10:29 +0530

    search added

--- src/app/pages/secure/home/home.page.ts
+++ src/app/pages/secure/home/home.page.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  content_loaded: boolean = false;
+  content_loaded = false;
 
   constructor() { }
+
+  handleSearch(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.content_loaded = false;
+    setTimeout(() => {
+      this.content_loaded = true;
+    }, 3000);
+  }
 
   ngOnInit() {
 
